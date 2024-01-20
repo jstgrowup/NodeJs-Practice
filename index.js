c2cca739c636d53916f1a00f8ef8ecccb7ff5c87
jstgrowup <deysubham999@gmail.com>
2024-01-21 00:36:46 +0530

    second

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const http = require("http");
 const fs = require("fs");
+const express = require("express");
+const app = express();
+app.get("/", (req, res) => {
+  return res.send("Hello from Home Page");
+});
+app.get("/about", (req, res) => {
+  return res.send("hello from about");
+});
 const myServer = http.createServer((req, res) => {
   const log = "new request appended";
   fs.appendFile("./log.txt", log, (err, data) => {
-    console.log('data:', data)
+    console.log("data:", data);
     res.end("hello from server ");
   });
 });
 
-myServer.listen(8000, () => {
+app.listen(8000, () => {
   console.log("server satrted");
 });
